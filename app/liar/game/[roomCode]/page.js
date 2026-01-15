5885178fb8d4ab66916830e18d54ae876d72c07d
scranu <scanu.working@gmail.com>
2026-01-15 15:32:36 +0100

    Update page.js

---
+++ app/liar/game/[roomCode]/page.js
@@ -0,0 +1,10 @@
+'use client';
+
+export default function LiarGamePage() {
+  return (
+    <main>
+      <h1>🎭 Lucky Liar Game</h1>
+      <p>Game page loaded successfully</p>
+    </main>
+  );
+}
